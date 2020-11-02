96fb99b56984ce0f82b973cda1fa96906a95413d
kitdaniellim <kitdaniellim@gmail.com>
2020-11-02 20:39:44 +0800

    updated

--- App.js
+++ App.js
@@ -1,63 +1,58 @@
-import React, { Component } from 'react';
-import { Alert, Text, AppRegistry, Button, TextInput, View, StyleSheet } from 'react-native';
-import Temp from './components/1_A_Temp.js'
-
-import { globalStyles } from './styles/styles';
-
-import { Constants } from 'expo';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+// import Selection from './components/2_SelectionPage.js';
+// import { LinearGradient } from  'expo-linear-gradient';
+import { Provider } from 'react-redux';
 import Routes from './routes/Routes.js';
-import firebase from "firebase"
-import Icon from 'react-native-vector-icons/FontAwesome';
-// import MenuButton from 'react-native-menu-button'
+import { decode, encode } from 'base-64'
 import { MenuProvider } from 'react-native-popup-menu';
-require("firebase/firestore");
+import firebase from "firebase"
+import configureStore from './store/configureStore';
+import 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAc9jbCdsAr25GlhpcLMWap-XBJy60Z2uE",
-  authDomain: "appointmentapp-d867d.firebaseapp.com",
-  databaseURL: "https://appointmentapp-d867d.firebaseio.com",
-  projectId: "appointmentapp-d867d",
-  storageBucket: "appointmentapp-d867d.appspot.com",
-  messagingSenderId: "346336386740",
-  appId: "1:346336386740:web:7f7788d6882e9b8c8d41e1",
-  measurementId: "G-32FB2FHN7B"
-};
+const store = configureStore();
 
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
+if (!global.btoa) { global.btoa = encode }
 
-// Make Auth and Firestore References
-//const auth = firebase.auth();
-const auth = firebase.auth();
-const db = firebase.firestore();
+if (!global.atob) { global.atob = decode }
 
-//Update Firestore Settings
-//db.settings( {timestampsInSnapshots: true} )
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {  //DONT REMOVE -Eldrin
-      username: '',
-      password: '',
-    };
+export default class App extends React.Component {
 
+  Home = (navigation) => {
+    //console.log("WEW MGA DUES")
+    navigation.navigate("Home");
   }
 
-  onLogin() {
-    const { username, password } = this.state;
+  componentDidMount() {
+    //alert("PUTANGNGNGGNGNNG")
 
-    Alert.alert('Credentials', `${username} + ${password}`);
-    auth.createUserWithEmailAndPassword(username, password);
+
+
+
+    //firebase.initializeApp(firebaseConfig);
+
+
+
+
+    // const Firebase = ({navigation}) => {}
+
   }
 
   render() {
+
+    //console.log("PUTANG INA NIYO PO")
+
+
+
     return (
-      <MenuProvider>
-        <Routes />
-      </MenuProvider>
+      <Provider store={store}>
+        <MenuProvider>
+          <Routes />
+        </MenuProvider>
+
+      </Provider>
     );
   }
 }
@@ -78,3 +73,8 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
 });
+
+export const db = firebase.firestore();
+
+
+
--- README.md
+++ README.md
@@ -7,6 +7,7 @@ To setup the project in the for local testing:
 This will install the dependencies needed for the project
 
 
+
 To run the project:
 
     npm start
